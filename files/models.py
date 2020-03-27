5e040e70dddb7654ddfc484c88d303be2a45ad62
Prabu <prabumohan96@gmail.com>
2020-03-27 16:42:50 +0530

    files added to django admin tool

--- files/models.py
+++ files/models.py
@@ -8,3 +8,6 @@ class Files(models.Model):
     id=models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False)
     file=models.CharField(null=False,max_length=255)
     user=models.ForeignKey(User,related_name='Files',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username+"_"+self.file
