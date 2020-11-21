f473fb8b8be930227d3223f15125f7262e9cda24
prabu.mohan <prabumohan96@gmail.com>
2020-11-21 15:43:57 +0530

    page size changed.

--- files/views.py
+++ files/views.py
@@ -18,7 +18,7 @@ from knox.auth import TokenAuthentication
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 100
 
