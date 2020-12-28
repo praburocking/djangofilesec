0d8426f3c4202231f6764fa25be04f9b9f2e6a90
prabu.mohan <prabumohan96@gmail.com>
2020-12-28 19:05:46 +0530

    build error fixed.

--- filesec/settings.py
+++ filesec/settings.py
@@ -188,6 +188,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'build', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dev_static'),
+   
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STRIPE_API_KEY=env('STRIPE_API_KEY')
